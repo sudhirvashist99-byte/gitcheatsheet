6bbb4737f5cd5c389daa929059177fa96a8b96df
sudhirvashist99-byte <sudhirvashist99@gmail.com>
2025-12-02 16:29:34 +0530

    Add script to generate MySQL Kubernetes secret
    This script retrieves MySQL credentials from AWS Secrets Manager and creates a Kubernetes secret manifest for MySQL.

---
+++ awssecretsindisekubernetes/generate-k8s-secret.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+SECRET=$(aws secretsmanager get-secret-value --secret-id myapp/mysql --query SecretString --output text)
+
+cat <<EOF > mysql-secret.yaml
+apiVersion: v1
+kind: Secret
+metadata:
+  name: mysql-secret
+type: Opaque
+data:
+  DB_HOST: $(echo $SECRET | jq -r '.DB_HOST' | base64 -w 0)
+  DB_USER: $(echo $SECRET | jq -r '.DB_USER' | base64 -w 0)
+  DB_PASSWORD: $(echo $SECRET | jq -r '.DB_PASSWORD' | base64 -w 0)
+  DB_NAME: $(echo $SECRET | jq -r '.DB_NAME' | base64 -w 0)
+EOF
+
+kubectl apply -f mysql-secret.yaml

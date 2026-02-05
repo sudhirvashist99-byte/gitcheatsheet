fb0811d074d96f9a881c3f294c6545d3294e0053
sudhirvashist99-byte <sudhirvashist99@gmail.com>
2026-02-05 12:45:28 +0530

    Update docker-install.sh

--- docker-install.sh
+++ docker-install.sh
@@ -29,4 +29,5 @@ sudo apt update -y
 
 echo "Done! You can now install Docker using:"
 echo "  sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin"
-
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+echo "Done! installation of Docker Completed"
